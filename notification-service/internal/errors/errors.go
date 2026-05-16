70addcae4ed108614b6608445637a63de92321d4
Said-Ahmad <behievs@gmail.com>
2026-05-16 18:33:47 +0300

    Подключил  middleware в сервисе уведомлений

--- notification-service/internal/errors/errors.go
+++ notification-service/internal/errors/errors.go
@@ -2,9 +2,9 @@ package errors
 
 import "errors"
 
-var ErrNotificationNotFound = errors.New("Уведомление не найдена")
-var ErrSubscriptionNotFound = errors.New("Подписка не найдена")
-var ErrSubscriptionTargetRequired = errors.New("Необходимо указать объект подписки")
+var ErrNotificationNotFound = errors.New("уведомление не найдено")
+var ErrSubscriptionNotFound = errors.New("подписка не найдена")
+var ErrSubscriptionTargetRequired = errors.New("необходимо указать объект подписки")
 var ErrTeamOrSportRequired = errors.New("необходимо выбрать один объект подписки")
-var ErrSubscriptionAlreadyExists = errors.New("Подписка уже существует")
+var ErrSubscriptionAlreadyExists = errors.New("подписка уже существует")
 var ErrUnauthorized = errors.New("не авторизован")
--- notification-service/internal/transport/routes.go
+++ notification-service/internal/transport/routes.go
@@ -1,10 +1,13 @@
 package transport
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Dukvaha27/flash-score/notification-service/internal/transport/middleware"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(router *gin.Engine, subscriptionHandler *SubscriptionHandler) {
 	subscription := router.Group("/subscription")
-
+	subscription.Use(middleware.InternalAuthMiddleware())
 	{
 		subscription.POST("", subscriptionHandler.Subscribe)
 		subscription.DELETE("/:subscriptionID", subscriptionHandler.Unsubscribe)
